b56252b0104c229473b53ea8413969892c911805
Niels Bertram <niels.bertram@fleurida.com>
2014-08-15 23:02:02 +1000

    - replicated camel dynamic router to create a zombie state

--- src/main/java/com/fleurida/camel/dyn/DynamicSourceRouter.java
+++ src/main/java/com/fleurida/camel/dyn/DynamicSourceRouter.java
@@ -2,16 +2,10 @@ package com.fleurida.camel.dyn;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.camel.BeanInject;
-import org.apache.camel.Body;
-import org.apache.camel.CamelContext;
 import org.apache.camel.DynamicRouter;
 import org.apache.camel.Exchange;
 import org.apache.camel.Header;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import com.fleurida.camel.session.ReleaseResourceRQ;
+import org.apache.camel.language.XPath;
 
 public class DynamicSourceRouter {
 
@@ -22,41 +16,59 @@ public class DynamicSourceRouter {
 
 	private String endpointPrefix = "direct-vm";
 
+	/**
+	 * A dynamic router that can be used to send a platform complient
+	 * 
+	 * @param previous
+	 *            the privious routing slip that is used to work out when to
+	 *            stop routing. If the previous routing slip is set to some
+	 *            value, this router will stop the routing
+	 * @param sourceAdapter
+	 *            the name of the platform adapter to route to
+	 * @param messageType
+	 *            the name of the root element node
+	 * @param messageVersion
+	 *            the version of the root element, aka message version
+	 * @param exchange
+	 *            the exchange that is to be routed in case we need anything
+	 *            from it
+	 * 
+	 * @return the dynamically constructed endpoint to route to
+	 */
 	@DynamicRouter
 	public String route(
-			@Header(DynamicSourceRouter.SOURCE_ADAPTER) String adapterName,
-			@Header(Exchange.SLIP_ENDPOINT) String previous, Exchange exchange) {
+			@Header(Exchange.SLIP_ENDPOINT) String previous,
+			@Header(DynamicSourceRouter.SOURCE_ADAPTER) String sourceAdapter,
+			@XPath(value = "/child::node()/local-name()", resultType = String.class) String messageType,
+			@XPath(value = "/child::node()/@version", resultType = String.class) String messageVersion,
+			Exchange exchange) {
 
 		// return as soon as we have advanced in the slip
 		if (previous != null) {
 			return null;
 		}
 
-		Document doc = exchange.getIn().getBody(Document.class);
+		if (messageType == null) {
+			// throw configuration error
+			throw new IllegalArgumentException(
+					"cannot determine message type from routed message");
 
-		if (doc != null && doc.getDocumentElement() != null) {
-			Element root = doc.getDocumentElement();
-			// need to make sure to grab only the local name
-			String rqType = root.getLocalName();
-
-			String version = root.getAttribute("version");
-			if (version == null || "".equals(version.trim())) {
-				log.warn("No request version found on {}, defaulting to 1.0",
-						rqType);
-				version = "1.0";
-			}
-
-			if (log.isDebugEnabled()) {
-				log.debug("Routing to {}:{}/{}-{}", endpointPrefix,
-						adapterName, rqType, version);
-			}
-
-			return String.format("%s:%s/%s-%s", endpointPrefix, adapterName,
-					rqType, version);
-
-		} else {
-			return null;
 		}
+
+		if (messageVersion == null || "".equals(messageVersion.trim())) {
+			log.warn("No request version found on {}, defaulting to 1.0",
+					messageType);
+			messageVersion = "1.0";
+		}
+
+		if (log.isDebugEnabled()) {
+			log.debug("Routing to {}:{}/{}-{}", endpointPrefix, sourceAdapter,
+					messageType, messageVersion);
+		}
+
+		return String.format("%s:%s/%s-%s", endpointPrefix, sourceAdapter,
+				messageType, messageVersion);
+
 	}
 
 	public static DynamicSourceRouter build() {
